95b4bcf79c85d9e7a13730a7bfa6554066590d24
Gleison <gleison04lemos@gmail.com>
2022-10-25 15:31:18 -0300

    feat: add templates e extends

--- task/urls.py
+++ task/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('Helloword/', views.helloWord),
+    path('', views.taskList, name='Task-list'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,14 @@
 from ctypes.wintypes import HACCEL
 from http.client import HTTPResponse
+from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def helloWord(request):
     return HttpResponse('Hello Word!')
+
+def taskList(request):
+    return render(request, 'task/list.html')
+
+def yourName(request, name):
+    return render(request, 'task/yourname.html', {'name': name})
