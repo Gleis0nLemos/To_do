2f319fcc0cb8a51e388ec5651c1e41a8ede29218
Gleison <gleison04lemos@gmail.com>
2022-10-26 15:23:50 -0300

    chore: add db in templates

--- task/views.py
+++ task/views.py
@@ -1,14 +1,20 @@
 from ctypes.wintypes import HACCEL
 from http.client import HTTPResponse
 from unicodedata import name
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Task
+
+def taskList(request):
+    tasks = Task.objects.all()
+    return render(request, 'task/list.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'task/task.html', {'task': task})
 
 def helloWord(request):
     return HttpResponse('Hello Word!')
-
-def taskList(request):
-    return render(request, 'task/list.html')
 
 def yourName(request, name):
     return render(request, 'task/yourname.html', {'name': name})
