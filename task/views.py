7258de3162fcd0aea355f09176fe5d6993e4e59d
Gleison <gleison04lemos@gmail.com>
2022-10-27 23:01:16 -0300

    chore: updating db from the front

--- task/views.py
+++ task/views.py
@@ -1,7 +1,3 @@
-from ctypes.wintypes import HACCEL
-from http.client import HTTPResponse
-from unicodedata import name
-from venv import create
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
@@ -28,6 +24,23 @@ def newTask(request):
     else:
         form = TaskForm()
         return render(request, 'task/addtask.html', {'form': form})
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            task.save()
+            return redirect('/')
+        
+        else:
+            return render(request, 'task/edittask.html', {'form': form, 'task': task})
+
+    else:
+        return render(request, 'task/edittask.html', {'form': form, 'task': task})
 
 def helloWord(request):
     return HttpResponse('Hello Word!')
